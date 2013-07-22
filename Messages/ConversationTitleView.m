59b4da7960e9aae746979e13f44edbd0e0798a91
Jesse Stuart <jesse@jessestuart.ca>
2013-07-22 16:42:52 -0400

    Add +n to conversation title avatars list

--- Messages/ConversationTitleView.m
+++ Messages/ConversationTitleView.m
@@ -16,7 +16,7 @@
     self = [super initWithFrame:frame];
     if (self) {
         int offset = 0;
-        for (int i=0; i<5; i++) {
+        for (int i=0; i<4; i++) {
             UIImage *avatar = [UIImage imageNamed:[NSString stringWithFormat:@"%d.jpg", i + 1]];
             avatar = [avatar thumbnailImage:29 transparentBorder:0 cornerRadius:3 interpolationQuality:kCGInterpolationHigh];
 
@@ -27,6 +27,20 @@
 
             offset = avatarView.frame.origin.x + avatarView.frame.size.width + 5;
         }
+
+        // +n view
+        UIView *nMore = [[UIView alloc] initWithFrame:CGRectMake(offset, 0, 29, 29)];
+        [nMore.layer setCornerRadius:3];
+        [nMore.layer setBorderColor:[UIColor whiteColor].CGColor];
+        [nMore.layer setBorderWidth:1.5f];
+        UILabel *nMoreText = [[UILabel alloc] initWithFrame:CGRectMake(1.5f, 1.5f, 29 - 3, 29 - 3)];
+        nMoreText.text = @"+2";
+        [nMoreText setBackgroundColor:[[UIColor alloc] initWithWhite:1 alpha:0]];
+        [nMoreText setTextColor:[[UIColor alloc] initWithWhite:1 alpha:1]];
+        [nMoreText setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:13]];
+        nMoreText.textAlignment = NSTextAlignmentCenter;
+        [nMore addSubview:nMoreText];
+        [self addSubview:nMore];
     }
     return self;
 }
