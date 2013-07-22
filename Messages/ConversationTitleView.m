12e1161d1805dce7de7d53594789c582947209f9
Jesse Stuart <jesse@jessestuart.ca>
2013-07-22 19:12:57 -0400

    Handle an arbitrary number of avatars in conversation view title

--- Messages/ConversationTitleView.m
+++ Messages/ConversationTitleView.m
@@ -15,38 +15,55 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
+        int avatarCount = 6;
+        int avatarSize = 29;
+        int avatarMarginRight = 5;
         int offset = 0;
-        for (int i=0; i<4; i++) {
+
+        int displayAvatarCount;
+        if (avatarCount > 5) {
+            displayAvatarCount = 4;
+        } else {
+            displayAvatarCount = avatarCount;
+
+            if (avatarCount < 5) {
+                offset = ((5 - displayAvatarCount) * (avatarSize + avatarMarginRight)) / 2;
+            }
+        }
+
+        for (int i=0; i<displayAvatarCount; i++) {
             UIImage *avatar = [UIImage imageNamed:[NSString stringWithFormat:@"%d.jpg", i + 1]];
-            avatar = [avatar thumbnailImage:29 transparentBorder:0 cornerRadius:3 interpolationQuality:kCGInterpolationHigh];
+            avatar = [avatar thumbnailImage:avatarSize transparentBorder:0 cornerRadius:3 interpolationQuality:kCGInterpolationHigh];
 
             UIView *avatarView = [[UIImageView alloc] initWithImage:avatar];
             avatarView.frame = CGRectMake(offset, 0, avatarView.frame.size.width, avatarView.frame.size.height);
 
             [self addSubview:avatarView];
 
-            offset = avatarView.frame.origin.x + avatarView.frame.size.width + 5;
+            offset = avatarView.frame.origin.x + avatarView.frame.size.width + avatarMarginRight;
         }
 
-        // +n view
-        UIView *nMore = [[UIView alloc] initWithFrame:CGRectMake(offset, 0, 29, 29)];
-        [nMore.layer setCornerRadius:3];
-        [nMore.layer setBorderColor:[UIColor whiteColor].CGColor];
-        [nMore.layer setBorderWidth:1];
-        UILabel *nMoreText = [[UILabel alloc] initWithFrame:CGRectMake(1.5f, 1.5f, 29 - 3, 29 - 3)];
-        nMoreText.text = @"+2";
-        [nMoreText setBackgroundColor:[[UIColor alloc] initWithWhite:1 alpha:0]];
-        [nMoreText setTextColor:[[UIColor alloc] initWithWhite:1 alpha:1]];
-        int nMoreFontSize;
-        if ([nMoreText.text length] > 3) {
-            nMoreFontSize = 10;
-        } else {
-            nMoreFontSize = 13;
+        if (avatarCount > 5) {
+            // +n view
+            UIView *nMore = [[UIView alloc] initWithFrame:CGRectMake(offset, 0, avatarSize, avatarSize)];
+            [nMore.layer setCornerRadius:3];
+            [nMore.layer setBorderColor:[UIColor whiteColor].CGColor];
+            [nMore.layer setBorderWidth:1];
+            UILabel *nMoreText = [[UILabel alloc] initWithFrame:CGRectMake(1.5f, 1.5f, avatarSize - 3, avatarSize - 3)];
+            nMoreText.text = [[NSString alloc] initWithFormat:@"+%d", avatarCount - displayAvatarCount];
+            [nMoreText setBackgroundColor:[[UIColor alloc] initWithWhite:1 alpha:0]];
+            [nMoreText setTextColor:[[UIColor alloc] initWithWhite:1 alpha:1]];
+            int nMoreFontSize;
+            if ([nMoreText.text length] > 3) {
+                nMoreFontSize = 10;
+            } else {
+                nMoreFontSize = 13;
+            }
+            [nMoreText setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:nMoreFontSize]];
+            nMoreText.textAlignment = NSTextAlignmentCenter;
+            [nMore addSubview:nMoreText];
+            [self addSubview:nMore];
         }
-        [nMoreText setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:nMoreFontSize]];
-        nMoreText.textAlignment = NSTextAlignmentCenter;
-        [nMore addSubview:nMoreText];
-        [self addSubview:nMore];
     }
     return self;
 }
