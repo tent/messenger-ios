29dfd84df96d5619c574b2ec3bd555c44edf9c5b
Jesse Stuart <jesse@jessestuart.ca>
2013-07-22 14:40:26 -0400

    Bottom toolbar in conversation view is always above keyboard

---
+++ Messages/ConversationViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConversationViewController.h
+//  Messages
+//
+//  Created by Jesse on 7/22/13.
+//  Copyright (c) 2013 Cupcake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConversationViewController : UIViewController
+
+@end
---
+++ Messages/ConversationViewController.m
@@ -0,0 +1,81 @@
+//
+//  ConversationViewController.m
+//  Messages
+//
+//  Created by Jesse on 7/22/13.
+//  Copyright (c) 2013 Cupcake. All rights reserved.
+//
+
+#import "ConversationViewController.h"
+
+@interface ConversationViewController ()
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *keyboardHeight;
+@end
+
+@implementation ConversationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    // Setup avatars in title
+    // UINavigationItem *navigationItem = self.navigationItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)observeKeyboard {
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillChangeFrameNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Begin observing the keyboard notifications when the view is loaded.
+    [self observeKeyboard];
+}
+
+// The callback for frame-changing of keyboard
+- (void)keyboardWillShow:(NSNotification *)notification {
+    NSDictionary *info = [notification userInfo];
+    NSValue *kbFrame = [info objectForKey:UIKeyboardFrameEndUserInfoKey];
+    NSTimeInterval animationDuration = [[info objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    CGRect keyboardFrame = [kbFrame CGRectValue];
+    
+    CGFloat height = keyboardFrame.size.height;
+
+    // Because the "space" is actually the difference between the bottom lines of the 2 views,
+    // we need to set a negative constant value here.
+    self.keyboardHeight.constant = -height;
+    
+    [UIView animateWithDuration:animationDuration animations:^{
+        [self.view layoutIfNeeded];
+    }];
+}
+
+- (void)keyboardWillHide:(NSNotification *)notification {
+    NSDictionary *info = [notification userInfo];
+    NSTimeInterval animationDuration = [[info objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    
+    self.keyboardHeight.constant = 0;
+    [UIView animateWithDuration:animationDuration animations:^{
+        [self.view layoutIfNeeded];
+    }];
+}
+
+@end
