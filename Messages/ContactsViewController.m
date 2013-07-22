f080ac9da1f1751f68d0cbb6bf9f32aaca7a21af
Jonathan Rudenberg <jonathan@titanous.com>
2013-07-21 21:04:51 -0400

    Clean up some warnings

--- Messages/ContactsViewController.m
+++ Messages/ContactsViewController.m
@@ -54,14 +54,12 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
     return _contactNames.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
     return 1;
 }
